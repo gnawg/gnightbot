ef903db3035acf25e19dd45b0535ccd43c5a761c
Gnawgy <gnawgy.gnawgy@gmail.com>
2019-11-17 23:09:22 -0500

    allow both user and query, and fix regex stuff

--- falljoke.js
+++ falljoke.js
@@ -1,8 +1,15 @@
 !addcom !falljoke
 
 $(eval
-  var name = '$(touser)'.replace(/^[a-z]/, '$(touser)'.charAt(0).toUpperCase());
-  var regex_vowel = /[aeiouy]/g;
+  var query = '$(query)';
+  var touser = '$(touser)';
+  if (query=="") {
+    var name = touser;
+  } else {
+    var name = query;
+  }
+  name = name.replace(/^[a-z]/, name.charAt(0).toUpperCase()).slice(0,30);
+  var regex_vowel = /[aeiouyAEIOUY]/g;
   var nameyell = name.replace(regex_vowel,'$&$&$&').toUpperCase();
   name.concat(", what's wrong? etalAwk ",name,"?! ",nameyell,"!! etalMad")
 )
